0af0d0d43434d9d689db23c00eb20798ce0c35db
seong <hst2570@gmail.com>
2024-12-24 16:04:13 +0900

    cron 하나로

--- batch/cron.ts
+++ batch/cron.ts
@@ -1,13 +1,49 @@
 import cron from 'node-cron'
 import gambling from '../src/strategy/gamblingWithList'
+import { run } from '../src/swap/ethUsdc'
+import { Balance } from '../src/types/@api'
+import { getMyAccount } from '../src/api'
+import { sendNotification } from '../src/notification'
 
-// running a task every day at 9:00 AM
+/**
+ * 매일 오전 9시에 gambling
+ */
 cron.schedule('0 9 * * *', () => {
   console.log('running a task every day at 9:00 AM')
   gambling.buy()
 })
 
-// running a task every 1 seconds
+/**
+ * 1초마다 실행, gambling
+ */
 cron.schedule('*/1 * * * * *', () => {
   gambling.sell()
+})
+
+/** 10분마다 실행, uniswap */
+cron.schedule('*/10 * * * *', () => {
+  run()
+})
+
+/**
+ * 매주 월요일 오전 9시에 내 계좌 잔고를 알려준다.
+ */
+cron.schedule('0 9 * * 1', () => {
+  const sendMyBalance = async () => {
+    const myAccount: Balance[] = await getMyAccount()
+
+    const allBalance = myAccount.reduce((acc, cur) => {
+      const { currency, balance, avg_buy_price } = cur
+
+      if (currency === 'KRW') {
+        return acc + balance
+      } else {
+        return acc + balance * avg_buy_price
+      }
+    }, 0)
+
+    sendNotification('[총 잔고] ' + allBalance.toLocaleString() + '원')
+  }
+
+  sendMyBalance()
 })
