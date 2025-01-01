78a20a51116f9575f27d983fa32567b6a705e239
seong <hst2570@gmail.com>
2025-01-02 00:01:34 +0900

    edit: cron

--- batch/runner/runSellCrypto.ts
+++ batch/runner/runSellCrypto.ts
@@ -1,7 +1,22 @@
 import gambling from '../../src/strategy/gamblingWithList'
 
 const run = () => async () => {
-  await gambling.sell()
+  let timeout = null as any
+  let interval = null as any
+  let isRunning = false
+
+  interval = setInterval(async () => {
+    if (isRunning) return
+
+    isRunning = true
+    await gambling.sell()
+    isRunning = false
+  }, 1000)
+
+  timeout = setTimeout(() => {
+    clearInterval(interval)
+    clearTimeout(timeout)
+  }, 60000)
 }
 
 run()
